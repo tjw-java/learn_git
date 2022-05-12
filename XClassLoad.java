956e4b6959dfc9a8ebc64103504037de7a233437
root <m15256947357_1@163.com>
2022-05-12 20:57:20 +0800

    第一周作业提交

---
+++ XClassLoad.java
@@ -0,0 +1,82 @@
+package com.tjw.java.jvm;
+
+
+import java.io.Closeable;
+import java.io.IOException;
+import java.io.InputStream;
+import java.lang.reflect.Method;
+
+
+public class XClassLoad extends ClassLoader {
+
+    public static void main(String[] args) throws Exception {
+        // 目标参数
+        final String className = "Hello";
+        final String methodName = "hello";
+        // 创建类加载器
+        ClassLoader classLoader = new XClassLoad();
+        // 加载相应的类
+        Class<?> clazz = classLoader.loadClass(className);
+        // 看看里面有些什么方法
+        boolean tag = false;
+        for (Method m : clazz.getDeclaredMethods()) {
+            if (m.getName().equals(methodName)) {
+                tag = true;
+            }
+            System.out.println(clazz.getSimpleName() + "." + m.getName());
+        }
+        // 创建对象
+        Object instance = clazz.getDeclaredConstructor().newInstance();
+        // 拥有目标函数才调用实例方法
+        if (tag) {
+            Method method = clazz.getMethod(methodName);
+            method.invoke(instance);
+        }
+    }
+
+    @Override
+    protected Class<?> findClass(String name) throws ClassNotFoundException {
+        // 如果支持包名, 则需要进行路径转换
+        String resourcePath = name.replace(".", "/");
+        // 文件后缀
+        final String suffix = ".xlass";
+        // 获取输入流
+        InputStream inputStream =
+                this.getClass().getClassLoader().getResourceAsStream(resourcePath + suffix);
+        try {
+            // 读取数据
+            assert inputStream != null;
+            int length = inputStream.available();
+            byte[] byteArray = new byte[length];
+            inputStream.read(byteArray);
+            // 转换
+            byte[] classBytes = decode(byteArray);
+            // 通知底层定义这个类
+            return defineClass(name, classBytes, 0, classBytes.length);
+        } catch (IOException e) {
+            throw new ClassNotFoundException(name, e);
+        } finally {
+            close(inputStream);
+        }
+    }
+
+    // 解码
+    private static byte[] decode(byte[] byteArray) {
+        byte[] targetArray = new byte[byteArray.length];
+        for (int i = 0; i < byteArray.length; i++) {
+            targetArray[i] = (byte) (255 - byteArray[i]);
+        }
+        return targetArray;
+    }
+
+    // 关闭
+    private static void close(Closeable res) {
+        if (null != res) {
+            try {
+                res.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
